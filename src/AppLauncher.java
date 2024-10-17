132ac69a6339622e5fa0cbbbbb3c19403b005396
Kushala C L <103378341+its-kushala@users.noreply.github.com>
2024-10-17 13:44:11 +0530

    Transaction added

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,19 +1,17 @@
-import db_objs.User;
-import guis.BankingAppGui;
+import guis.LoginGui;
 
 import javax.swing.*;
-import java.math.BigDecimal;
 
 public class AppLauncher {
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-             //   new LoginGui().setVisible(true);
+               new LoginGui().setVisible(true);
               //  new RegisterGui().setVisible(true);
-                new BankingAppGui(
+               /* new BankingAppGui(
                         new User(1, "username", "password", new BigDecimal("20.00"))
-                ).setVisible(true);
+                ).setVisible(true); */
             }
         });
     }
